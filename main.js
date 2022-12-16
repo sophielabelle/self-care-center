fd0d06545a6c010023d91b069b2820b3f9685a16
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-12-16 08:33:08 -0800

    Finish out the add random message

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Variables
-var radioBtns = document.querySelectorAll('input[name="radio"]')
 var affirmBtn = document.querySelector('#radio1')
 var mantraBtn = document.querySelector('#radio2')
 
@@ -7,23 +6,22 @@ var receiveMessageBtn = document.querySelector('#receive-btn');
 var meditateImg = document.querySelector('.meditate-img')
 
 var message = document.querySelector('#message-view');
+
+
 // Event Listener
-
 receiveMessageBtn.addEventListener('click', displayMessage);
-
+// event listener for the fav button pressed 
 
 // Functions----------------------------------
-// show message in 2nd box when button is clicked and removes the meditation image from box
 
 function displayMessage() {
-    for (var i = 0; i < radioBtns.length; i++) {
-        if (radioBtns[i].checked) {
-            message.innerHTML = affirmations[randomMessage(affirmations)];
-        } else if (radioBtns[i].checked) {
-            message.innerHTML = mantras[randomMessage(mantras)];
-        };
-    };
+    if(affirmBtn.checked) {
+        message.innerHTML = affirmations[randomMessage(affirmations)]
+    } else if (mantraBtn.checked) {
+        message.innerHTML = mantras[randomMessage(mantras)];
+    }
 };
+
 
 function randomMessage(array) {
     return Math.floor(Math.random() * array.length);
